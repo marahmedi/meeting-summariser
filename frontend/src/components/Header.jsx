fc1359704ce51df157bbd6324fab480edc2655ab
ayubkk <ayubkaou@gmail.com>
2024-02-29 19:37:18 +0000

    Updated containers and CSS

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Header.css'
+
+
+const Header = () => {
+  return (
+    <div className='HeaderContainer'>
+      <h1 className='Header'>Upload and attach your meeting audio below!</h1>
+    </div>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/UploadBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+const UploadBox = () => {
+      return (
+    <div className='upload-box'>
+      <div className='file-input'>Input file here</div>
+      <div className='submit-button'>Submit</div>
+      <div className='loading-indicator'>Loading Indicator...</div>
+      <div className='upload-box'>
+      <div className='loading'>Loading</div>
+      </div>
+    </div>
+  )
+}
+
+export default UploadBox
