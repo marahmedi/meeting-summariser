29b6809b49b9a30ee802a1b812c1487abeab90e6
ayubkk <ayubkaou@gmail.com>
2024-03-08 17:02:17 +0000

    added loading bar and reset the file input value after upload

--- frontend/src/components/UploadBox.jsx
+++ frontend/src/components/UploadBox.jsx
@@ -2,23 +2,28 @@ import React from 'react'
 import '../styles/UploadBox.css'
 import image from '../assets/uploadImage.png'
 import { useState } from 'react'
+import LoadingBar from 'react-top-loading-bar';
 
 const UploadBox = () => {
     const [file, setFile] = useState(null);
+    const [progress, setProgress] = useState(0);
 
     const handleFileChange = (e) => {
       if (e.target.files) {
         const selectedFile = e.target.files[0];
         setFile(selectedFile);
-  
+        setProgress(20); // Set progress of bar
         // Upload the file immediately (you can customize this logic)
         uploadFile(selectedFile);
+        setProgress(100);
+        e.target.value = null; // Reset the file input value after upload
       }
     };
   
     const uploadFile = (fileToUpload) => {
      
       console.log('Uploading file:', fileToUpload.name);
+      setProgress(30);
       fetch('https://httpbin.org/post', {
         method: 'POST',
         body: fileToUpload, // Set headers manually for single file upload
@@ -30,6 +35,7 @@ const UploadBox = () => {
        .then((response) => response.json())
        .then((data) => console.log(data))
        .catch((error) => console.error(error));
+       setProgress(50);
     };
       return (
           <div className='UploadBox'>
@@ -45,6 +51,11 @@ const UploadBox = () => {
               <p className='UploadText'>Click here to upload your file! 📂</p>
               </label>
               <h2 className='MaxFileSize'> Max file size: 10MB </h2>
+              <LoadingBar
+        color='#f11946'
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
           </div>
       )
 }
