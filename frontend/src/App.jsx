ea445e9ff53d468597948ed6c4d540471d710ca3
ayubkk <ayubkaou@gmail.com>
2024-03-28 12:21:40 +0000

    fixed marah's mistakes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,20 +3,11 @@ import UploadBox from './components/UploadBox'
 import Header from './components/Header'
 
 function App() {
-<<<<<<< HEAD
-  
-
-  return (
-    <>
-      hey
-    </>
-=======
   return (
     <div className="App">
     <Header/>
     <UploadBox/>
     </div>
->>>>>>> e93627fe3165abbbfada1fca45f106421e3be44f
   )
 }
 
