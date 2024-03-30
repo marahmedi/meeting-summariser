f825b0219a341fd8cd51d51f67325255375ad27f
ayubkk96 <ayubkaou@gmail.com>
2024-03-30 14:40:12 +0000

    Wowzers, we can now send a video or audio POST request from the frontend and the OPEN AI Whisper model will successfully produce a summary of the video! It even shows it on our frontend!

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
+from audio_processing.transcribe_audio import transcribe_audio
+from audio_processing.trim_audio import refactor_audio
+from openai_integration.openai_client import create_openai_client
+from openai_integration.summarisation import (abstract_summary_extraction)
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -18,15 +22,26 @@ def upload_audio_file():
             return jsonify({'error': 'No selected audio file'}), 400
 
         # Replace 'audio_uploads' with your desired directory for audio files
-        # audio_file.save('../' + audio_file.filename)
-        audio_file.save('' + audio_file.filename)
+        audio_file.save('./' + audio_file.filename)
+        summary = summarise_audio(audio_file)
         print('Audio file saved successfully')
-        messages = ["Hello from our server", "Hi from our server", 
-                    "Greetings from our server", "Welcome from our server",
-                      "Good day from our server"]
+
+        messages = [summary]
         return jsonify(messages), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+
+def summarise_audio(audio_file):
+    try:
+        refactor_audio('./' + audio_file.filename, 'Audio_files')
+        client = create_openai_client()
+        transcription = transcribe_audio(client, './Audio_files')
+        summary = abstract_summary_extraction(client, transcription)
+        print(summary)
+        return summary
+    except Exception as e:
+        return str(e)
 
 
 if __name__ == '__main__':
